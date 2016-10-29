a6b0b75bf798571338ad30194060cf6130e0f24f
Jesse Rong <jbr5205@psu.edu>
2016-10-29 06:24:01 -0400

    Fixed Error

--- textmessager.py
+++ textmessager.py
@@ -42,28 +42,28 @@ def sms():
                  21
                  22
                  23
-                 24:"Residents have access to essential health care. ";
-                 25:"Survivors of slavery receive appropriate government compensation according to law.";
-                 26:"Child survivors of slavery are attending school.";
-                 27:"Adult survivors of slavery are earning a livelihood comparable to others in this community.";
-                 28:"Survivors of slavery are accessing essential health care.";
-                 29:"Survivors of slavery can access support for psychological trauma.";
-                 30:"Survivors of slavery are protected from community-wide stigma.";
-                 31:"There is an anti-slavery community group that meets regularly.";
-                 32:"The community group has good leadership.";
-                 33:"Slavery survivors participate effectively in the group.";
-                 34:"Poorer households participate effectively in the group.";
-                 35:"Discriminated groups participate effectively in the group.";
-                 36:"Women participate effectively in the group.";
-                 37:"The community group has strong internal cohesion.";
-                 38:"The community group is well accepted within the community (while recognizing that those connected with slaveholders and trafficking may not accept the group).";
-                 39:"The group can resolve internal disagreements and maintain unity and trust.";
-                 40:"The group makes its own decisions, without external pressure. ";
-                 41:"The group develops good plans for keeping the village free from trafficking and slavery.";
-                 42:"The group is effective at implementing its plans. ";
-                 43:"All members participate equitably in carrying out the work of the group.";
-                 44:"The group is effective at advocacy with local authorities.";
-                 45:"The group is effective at reducing slavery in the community. ";
+                 24:"Residents have access to essential health care. ",
+                 25:"Survivors of slavery receive appropriate government compensation according to law.",
+                 26:"Child survivors of slavery are attending school.",
+                 27:"Adult survivors of slavery are earning a livelihood comparable to others in this community.",
+                 28:"Survivors of slavery are accessing essential health care.",
+                 29:"Survivors of slavery can access support for psychological trauma.",
+                 30:"Survivors of slavery are protected from community-wide stigma.",
+                 31:"There is an anti-slavery community group that meets regularly.",
+                 32:"The community group has good leadership.",
+                 33:"Slavery survivors participate effectively in the group.",
+                 34:"Poorer households participate effectively in the group.",
+                 35:"Discriminated groups participate effectively in the group.",
+                 36:"Women participate effectively in the group.",
+                 37:"The community group has strong internal cohesion.",
+                 38:"The community group is well accepted within the community (while recognizing that those connected with slaveholders and trafficking may not accept the group).",
+                 39:"The group can resolve internal disagreements and maintain unity and trust.",
+                 40:"The group makes its own decisions, without external pressure.",
+                 41:"The group develops good plans for keeping the village free from trafficking and slavery.",
+                 42:"The group is effective at implementing its plans.",
+                 43:"All members participate equitably in carrying out the work of the group.",
+                 44:"The group is effective at advocacy with local authorities.",
+                 45:"The group is effective at reducing slavery in the community. ",
                  46:"The group has built strong links with other anti-slavery community groups."
                  }
 
