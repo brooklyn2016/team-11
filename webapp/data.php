053f95be39b0539d50eb48737b96b0aee3b3367c
Jesse Rong <jbr5205@psu.edu>
2016-10-29 05:08:31 -0400

    Reading reports and displaying them test

--- webapp/data.php
+++ webapp/data.php
@@ -50,3 +50,21 @@
       <script src="../assets/js/custom.js"></script>
       <script type="text/javascript">/* <![CDATA[ */(function(d,s,a,i,j,r,l,m,t){try{l=d.getElementsByTagName('a');t=d.createElement('textarea');for(i=0;l.length-i;i++){try{a=l[i].href;s=a.indexOf('/cdn-cgi/l/email-protection');m=a.length;if(a&&s>-1&&m>28){j=28+s;s='';if(j<m){r='0x'+a.substr(j,2)|0;for(j+=2;j<m&&a.charAt(j)!='X';j+=2)s+='%'+('0'+('0x'+a.substr(j,2)^r).toString(16)).slice(-2);j++;s=decodeURIComponent(s)+a.substr(j,m-j)}t.innerHTML=s.replace(/</g,'&lt;').replace(/>/g,'&gt;');l[i].href='mailto:'+t.value}}catch(e){}}}catch(e){}})(document);/* ]]> */</script></body>
       </html>
+	  
+<?php
+
+$fileR = fopen("OutputFile.txt", "r") or die("Unable to open"); 
+$FileContents= fread($fileR, filesize($fileR));
+fclose($fileR);
+
+$dataOutput = json_decode($FileContents, true);
+
+echo "Organization: $dataOutput['organization']";
+echo "Village/Settlement: $dataOutput['settlement']";
+echo "District: $dataOutput['district']";
+echo "Date this assessment was completed: $dataOutput['datecompleted']";
+echo "Estimated population: $dataOutput['estimated_population']";
+echo "Traffickers, whether from the village or from outside the village, cannot operate any more: $dataOutput['A1']";
+echo "Question 1 comment: $dataOutput['A1textarea']";
+
+?>
